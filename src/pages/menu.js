5a561790d8def172cd7e0fc094a8051fed433ac5
Suxxeed <f.wollenzien@gmail.com>
2020-08-18 17:16:17 +0200

    deleted menupage temporarily

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,25 +1,12 @@
 import React, { useState } from "react"
-import { Document, Page } from "react-pdf"
 
 import MenuPDF from "../../content/assets/Speisekarte.pdf"
 
 function MyApp() {
-  const [numPages, setNumPages] = useState(null)
-  const [pageNumber, setPageNumber] = useState(1)
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages)
-  }
-  console.log("PDF:" + MenuPDF)
 
   return (
     <div>
-      <Document file="../../content/assets/Speisekarte.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
+        <h1>Test</h1>
     </div>
   )
 }
