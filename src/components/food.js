b0ed33c05595ea87655bd8e79d195fd2160ec0d8
Suxxeed <f.wollenzien@gmail.com>
2023-05-26 11:48:00 +0200

    UPDATE height of pictures food.js

--- src/components/food.js
+++ src/components/food.js
@@ -31,19 +31,19 @@ const Food = () => {
     <div id="food">
       <Slider {...settings}  id="slider">
         <div>
-          <img src={FoodPic1} alt="Eventlocation Ochsen Luftaufnahme" />
+          <img src={FoodPic1} alt="Eventlocation Ochsen Luftaufnahme" height="1234" />
         </div>
         <div>
-          <img src={FoodPic2} alt="der große Saal im Ochsen" />
+          <img src={FoodPic2} alt="der große Saal im Ochsen" height="1234" />
         </div>
         <div>
-          <img src={FoodPic3} alt="gute Stube des Museumsgasthofs" />
+          <img src={FoodPic3} alt="gute Stube des Museumsgasthofs" height="1234" />
         </div>
         <div>
-          <img src={FoodPic4} alt="Stubeeingang" />
+          <img src={FoodPic4} alt="Stubeeingang" height="1234" />
         </div>
         <div>
-          <img src={FoodPic5} alt="Nebenraum Ratsherrenzimmer" />
+          <img src={FoodPic5} alt="Nebenraum Ratsherrenzimmer" height="1234" />
         </div>
       </Slider>
     </div>
